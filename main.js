5839b5ceae64694ead809cb6a15f9bec48557195
川田隼輔 <kwt20238120@gmail.com>
2023-05-22 23:31:08 +0900

    update html要素の編集 && ログの表示形式の変更

--- main.js
+++ main.js
@@ -12,10 +12,23 @@ const analyze_sentence = async () => {
       // 解析結果を順番に取得する
       result = token;
       result_array.push([result.surface_form, result.pos]);
+      console.log([result.surface_form, result.pos]);
     });
   });
   console.log(result_array);
   return result_array;
 };
 
-const display_result = async () => {};
+const display_result = async () => {
+  // while (result_div?.firstChild) {
+  //   container.removeChild(result_div?.firstChild);
+  // }
+  const result_array = await analyze_sentence();
+  let result_ul = document.getElementById("result_ul");
+  result_array.forEach((item) => {
+    const list_item = document.createElement("li");
+    list_item.textContent = item[0] + "," + item[1];
+    result_ul.appendChild(list_item);
+    console.log(list_item);
+  });
+};
