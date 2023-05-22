e8552ed57cf6a43bbaf67ff7fc0c350a9f32b1f5
川田隼輔 <kwt20238120@gmail.com>
2023-05-22 21:01:01 +0900

    update 表示されないエラーの解消

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 const DICT_PATH = "./dict";
 
-function analyze_sentence() {
+async function analyze_sentence() {
   let sentence = document.getElementById("sentenceToAnalyze").value;
   console.log("start");
   console.log(sentence);
-  kuromoji.builder({ dicPath: DICT_PATH }).build((err, tokenizer) => {
+  let tmp = "";
+  await kuromoji.builder({ dicPath: DICT_PATH }).build((err, tokenizer) => {
     const tokens = tokenizer.tokenize(sentence); // 解析データの取得
     tokens.forEach((token) => {
       // 解析結果を順番に取得する
-      console.log(token);
+      // console.log(token);
+      tmp = token?.surface_form + "," + token?.pos;
     });
   });
+
+  console.log(tmp);
 }
