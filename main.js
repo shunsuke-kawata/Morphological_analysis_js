be702f1beefd25d9321e0a70dcf3cc4224feff6d
川田隼輔 <kwt20238120@gmail.com>
2023-05-22 22:59:50 +0900

    update ログの表示形式を変更

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
 const DICT_PATH = "./dict";
 
-async function analyze_sentence() {
+const analyze_sentence = async () => {
   let sentence = document.getElementById("sentenceToAnalyze").value;
   console.log("start");
   console.log(sentence);
-  let tmp = "";
+  let result;
+  let result_array = [];
   await kuromoji.builder({ dicPath: DICT_PATH }).build((err, tokenizer) => {
     const tokens = tokenizer.tokenize(sentence); // 解析データの取得
     tokens.forEach((token) => {
       // 解析結果を順番に取得する
-      console.log(token);
+      result = token;
+      result_array.push([result.surface_form, result.pos]);
     });
   });
-}
+  console.log(result_array);
+  return result_array;
+};
+
+const display_result = async () => {};
